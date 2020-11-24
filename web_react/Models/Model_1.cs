e1ad4a5b5d226660af33101f865cc90c5edbe797
karmannik <vlad.urbanovitch@yandex.by>
2020-11-24 22:53:55 +0300

    Start project

---
+++ web_react/Models/Model_1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace web_react.Controllers.Models
+{
+    public class Model_1
+    {
+        public int id { get; set; }
+
+        public string value { get; set; }
+        
+    }
+}
